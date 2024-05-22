4c92aee1f2d186b929a7260daa2fd5b768b4a393
DangNH <nguyenhuudang0206@gmail.com>
2024-05-22 09:48:52 +0700

    Initial project

---
+++ MyEBook/MyEBook.Tests/TestBookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace MyEBook.Tests
+{
+    [TestClass]
+    public class TestBookController
+    {
+        [TestMethod]
+        public void TestMGetBook()
+        {
+            
+        }
+
+        
+    }
+}
---
+++ MyEBook/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a70eac6e-2914-4d6c-905f-23904dd382a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
